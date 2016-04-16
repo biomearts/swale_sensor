73c71b19685128886185842d808312b686c779f5
brianhouse <brian.house@gmail.com>
2016-04-16 10:35:30 -0400

    completed and threaded for single sensor

--- main.py
+++ main.py
@@ -1,70 +1,56 @@
 #!/usr/bin/env python3
 
-import time, json, math
+import time, json, math, threading, queue
 from random import random
 from collections import deque, OrderedDict
-from housepy import config, log, util, animation
+from housepy import config, log, util, net
 from housepy.xbee import XBee
-from mongo import db
-
-sensor_data = {}
-sensor_rssi = OrderedDict()
-labels = []
 
 RANGE = 0, 1023
-RANGE = 300, 723
-
 TYPE = "moisture"
 
 def message_handler(response):
-    # log.info(response)
     try:
-        print(response['sensor'], response['samples'], response['rssi'])
-        t = util.timestamp(ms=True)        
+        # print(response['sensor'], response['samples'], response['rssi'])
+        t_utc = util.timestamp(ms=True)        
         sensor = response['sensor']
-        sample = response['samples']  ## just one dimension?
-        sample.append(round(sum(sample) / 3))
+        sample = response['samples']
         rssi = response['rssi']
-        data = {'t': t, 'type': TYPE, 'sensor': sensor, 'sample': sample, 'rssi': rssi}
-        # print(json.dumps(data, indent=4))
-        db.branches.insert(data)
-        if sensor not in sensor_data:
-            sensor_data[sensor] = deque()
-            sensor_rssi[sensor] = None
-        sensor_data[sensor].appendleft((t, sample))
-        sensor_rssi[sensor] = t, rssi
-        if len(sensor_data[sensor]) == 1000:
-            sensor_data[sensor].pop()
+        data = {'t_utc': t_utc, 'type': TYPE, 'sensor': sensor, 'sample': sample, 'rssi': rssi}
+        data_sender.queue.put(data)
     except Exception as e:
         log.error(log.exc(e))
 
-def draw():
-    t_now = util.timestamp(ms=True)
 
-    # ctx.line3(0., 0., 0., .5, .5, .5)
-    # ctx.line(0., 0., .5, .5)
+class DataSender(threading.Thread):
 
-    for s, (sensor, (t, rssi)) in enumerate(sensor_rssi.items()):
-        if t_now - t > 3:
-            bar = 0.01
-        else:
-            bar = 1.0 - (max(abs(rssi) - 25, 0) / 100)
-        x = (20 + (s * 20)) / ctx.width
-        ctx.line(x, .1, x, (bar * 0.9) + .1, color=(0., 0., 0., 0.5), thickness=10)
-        if sensor not in labels:
-            print("Adding label for sensor %s" % sensor)
-            labels.append(sensor)
-            ctx.label(x, .05, str(sensor), font="Monaco", size=10, width=10, center=True)
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.queue = queue.Queue()
+        self.start()
 
-    for sensor in list(sensor_data):
-        samples = sensor_data[sensor]
-        if len(samples):
-            # ctx.lines([((t_now - sample[0]) / 10.0, (sample[1][0] - RANGE[0]) / (RANGE[1] - RANGE[0])) for sample in list(samples)], color=(1., 0., 0., 1.))
-            # ctx.lines([((t_now - sample[0]) / 10.0, (sample[1][1] - RANGE[0]) / (RANGE[1] - RANGE[0])) for sample in list(samples)], color=(0., 1., 0., 1.))
-            ctx.lines([((t_now - sample[0]) / 10.0, (sample[1][2] - RANGE[0]) / (RANGE[1] - RANGE[0])) for sample in list(samples)], color=(0., 0., 1., 1.))
-            # ctx.lines([((t_now - sample[0]) / 10.0, (sample[1][3] - RANGE[0]) / (RANGE[1] - RANGE[0])) for sample in list(samples)], color=(0., 0., 0., 1.))
+    def run(self):
+        while True:  
+            data = []
+            try:
+                start_t = time.time()
+                while True:
+                    data.append(self.queue.get_nowait())
+            except queue.Empty:
+                if len(data):
+                    ## doesnt take into account multiple sensors
+                    average_sample = round(sum([d['sample'][0] for d in data]) / len(data)) / RANGE[-1]
+                    average_rssi = round(sum([d['rssi'] for d in data]) / len(data))
+                    entry = {'t_utc': util.timestamp(), 'type': TYPE, 'moisture': average_sample, 'rssi': average_rssi}
+                    log.info(json.dumps(entry, indent=4))
+                    try:
+                        net.read(config['server'], data=entry, timeout=5)
+                    except Exception as e:
+                        log.error(log.exc(e))
+            while time.time() - start_t < 5.0:
+                time.sleep(0.02)
 
 
-xbee = XBee(config['device_name'], message_handler=message_handler)
-ctx = animation.Context(1000, 300, background=(1., 1., 1., 1.), fullscreen=False, title="SWALE", smooth=True)    
-ctx.start(draw)
+data_sender = DataSender()
+xbee = XBee(config['device_name'], message_handler=message_handler, blocking=True)
